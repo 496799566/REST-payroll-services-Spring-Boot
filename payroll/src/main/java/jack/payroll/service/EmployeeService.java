49b7e48e7ea11edd209f1c6ed7407ed502e2572f
Jack Chen <44731328+496799566@users.noreply.github.com>
2020-07-01 21:04:45 -0400

    Update EmployeeService.java
    Simplified HATEOAS link creation by injecting the EmployeeAssembler singleton and calling the toModel method.

--- payroll/src/main/java/jack/payroll/service/EmployeeService.java
+++ payroll/src/main/java/jack/payroll/service/EmployeeService.java
@@ -8,6 +8,7 @@ import org.springframework.hateoas.Link;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.stereotype.Service;
 
+import jack.payroll.assembler.EmployeeAssembler;
 import jack.payroll.controller.EmployeeController;
 import jack.payroll.exception.EmployeeNotFoundException;
 import jack.payroll.model.Employee;
@@ -19,49 +20,47 @@ public class EmployeeService {
 	@Autowired
 	private EmployeeRepository repo ;
 	
+	@Autowired
+	private EmployeeAssembler assembler;
+	
 	public List<Employee> getAllEmployees() {
 		
-		List<Employee> employeeList = repo.findAll();
-		
-		return employeeList.stream()
-				.map( employee -> {
-					employee.add( WebMvcLinkBuilder.linkTo( getController().getEmployeeById(employee.getId())	).withSelfRel());
-					employee.add( WebMvcLinkBuilder.linkTo( getController().getAll()							).withRel("employees"));
-					return employee;
-				})
-				.collect(Collectors.toList());
+		return repo.findAll() // list of Employee object from the database
+				.stream()
+				.map( employee -> assembler.toModel(employee) ) // add HATEOAS links, self and employees
+				.collect( Collectors.toList() );
 	}
 	
 	public Employee getEmployeeById(Long id) {
+
 		Employee employee = repo.findById(id).orElseThrow(() -> new EmployeeNotFoundException(id));
+		return assembler.toModel(employee);
 		
-		Link self 		= WebMvcLinkBuilder.linkTo( getController().getEmployeeById(id)	).withSelfRel();
-		Link employees 	= WebMvcLinkBuilder.linkTo( getController().getAll()			).withRel("employees");
-		
-		employee.add( self );
-		employee.add( employees );
-		
-		return employee;
 	}
 	
-	private EmployeeController getController() {
-		return WebMvcLinkBuilder.methodOn(EmployeeController.class);
-	}
 	
 	public Employee addNewEmployee(Employee employee) {
+		
 		return repo.save(employee);
+		
 	}
 	
 	public Employee updateEmployeeById(Employee employee, Long id) {
+		
 		if ( repo.existsById(id) ) {
 			employee.setId(id);
 			return repo.save(employee);
 		} else {
 			throw new EmployeeNotFoundException(id);
 		}
+		
 	}
 	
 	public void deleteEmployeeById(Long id) {
+		
 		repo.deleteById(id);
+		
 	}
+	
 }
+
